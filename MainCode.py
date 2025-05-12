4c64becf1ba36eedd3cfaa40cc7400c45d507695
william174-ux3 <william.eames@papamoacollege.school.nz>
2025-05-13 09:30:10 +1200

    Add files via upload

---
+++ MainCode.py
@@ -0,0 +1,22 @@
+# description of what you need to do
+# and how to play
+print("Hello, welcome to my maths quiz!")
+print("Answer these basic facts questions...")
+
+# asks how many rounds they would like to do
+ask_user = input("How many questions do you want to answer? ")
+
+
+# questions to ask the user
+
+
+# producing the random number equations
+
+
+# check the users answers if they are right or wrong
+
+
+# asking if you want to see the game history
+
+
+# producing the game history
