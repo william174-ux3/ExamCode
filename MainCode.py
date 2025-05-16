ff8da6d16cb7f2da130c825fceba01386003ce28
william174-ux3 <william.eames@papamoacollege.school.nz>
2025-05-16 14:50:45 +1200

    Add files via upload

--- MainCode.py
+++ MainCode.py
@@ -1,10 +1,9 @@
-#DATE 15/05/2025
-#PROGRAM Maths Quiz
-#AUTHOR William Eric Eames
+# DATE 15/05/2025
+# PROGRAM Maths Quiz
+# AUTHOR William Eric Eames
 
-# inputs the random library
+# imports the random library
 import random
-from operator import index
 
 # initialise game variables for game history
 questions_answered = 0
@@ -24,10 +23,10 @@ for index in range(num_questions):
     num1 = random.randint(1, 12)
     num2 = random.randint(1, 12)
 
-# asking the question
+    # asking the question
     answer = int(input(f"Question {index + 1}: What is {num1} + {num2}? "))
 
-# checking the answer
+    # checking the answer
     if answer == num1 + num2:
         print("Correct! \n")
         questions_correct += 1
@@ -35,23 +34,30 @@ for index in range(num_questions):
         print(f"Oh no! The correct answer is {num1 + num2}!\n")
         questions_wrong += 1
 
+    # count this question as the user answers it
+    questions_answered += 1
+
+# checking to see if the answer for the number of
+# questions are only numbers
+
+
+# checking to see if the answer for the questions
+# are numbers
+
+
+# calculate the percentages for the statistics
+percent_correct = (questions_correct / questions_answered) * 100
+percent_wrong = (questions_wrong / questions_answered) * 100
+
 # asking if you want to see the game history
-game_history = input("Do you want to see your game history? yes/no ")
-
-# adding onto the questions_answered variable
-questions_answered += 1
-
-# calculate game history
-questions_correct = questions_answered - questions_correct - questions_wrong
-percent_won = questions_correct / questions_answered * 100
-percent_lost = questions_wrong / questions_answered * 100
-percent_tied = 100 - percent_won - percent_lost
+game_history = input("Do you want to see your game history? yes/no ").lower()
 
 # producing the game history
 if game_history == "yes":
-    print("📈📈📈 Game Statistics 📈📈📈")
-    print(f" Won: {percent_won:.2f} \t "
-          f" Lost: {percent_lost:.2f}\n")
+    print("\n📈📈📈 Game Statistics 📈📈📈")
+    print(f"Questions Answered: {questions_answered}")
+    print(f"Correct Answers: {questions_correct} ({percent_correct:.2f}%)")
+    print(f"Wrong Answers: {questions_wrong} ({percent_wrong:.2f}%)\n")
 
 # output for after the game history
 print("👍👍👍 Thanks for playing my maths quiz 👍👍👍")
